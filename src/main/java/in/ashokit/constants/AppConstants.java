38523c64833d3d0689f8924e830623b589842e38
Saurabh@3255 <saurabhpshelke@gmail.com>
2023-08-20 21:03:15 +0530

    Second ommit

---
+++ src/main/java/in/ashokit/constants/AppConstants.java
@@ -0,0 +1,22 @@
+package in.ashokit.constants;
+
+public class AppConstants.PLAN_UPDATED_SAVE);
+{
+	public static final String EMPTY_STR=""; 
+
+	public static final String PLAN_SAVE_SUCC="planSaveSucc"; 
+	
+	public static final String PLAN_SAVE_FAIL="planSaveFailed";
+	
+	public static final String PLAN_UPDATED_SAVE="planUpdatedSucc";
+	
+	public static final String PLAN_UPDATED_FAIL="planUpdatedFail";
+	
+	public static final String PLAN_DELETED_SUCC="planDeleteSucc";
+	
+	public static final String PLAN_DELETED_FAIL="planDeleteFail";
+	
+	public static final String PLAN_STATUS_CHANGE_SUCC ="planStatusChnageSucc";
+	
+	public static final String PLAN_STATUS_CHANGe_FAIL ="planStatusChangeFailed";
+}
